ec6c2b49f680e15fd328ecba6dae70b3c9d466e2
Blue-Lemonz <116973765+Blue-Lemonz@users.noreply.github.com>
2022-10-30 19:56:12 +0800

    Add files via upload

---
+++ main.js
@@ -0,0 +1,14 @@
+function start()
+{
+    window.location = "gamePage.html";
+}
+
+function howToPlay()
+{
+    window.location = "instructions.html";
+}
+
+function back()
+{
+    window.location = "index.html";
+}
